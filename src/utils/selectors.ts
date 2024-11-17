9da53f324622a1d4ec55fa0ea95724617a3fed49
Filipe Dávila <filipedavila6@gmail.com>
2024-11-17 20:50:51 +0000

    Added selectors for website of chatbot

--- src/utils/selectors.ts
+++ src/utils/selectors.ts
@@ -1,5 +1,7 @@
 export const ChatPlatformSelectors = {
   'https://chatgpt.com': 'div.agent-turn',
   'https://claude.ai': 'div.font-claude-message',
-  'https://gemini.google.com': 'message-content.model-response-text'
+  'https://gemini.google.com': 'message-content.model-response-text',
+   'https://www.continente.pt': '[c-botchatmessage_botchatmessage-host]'
 };
+
