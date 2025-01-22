1e70fadc42487a67494a065f14bb2c27da5d6aac
Filipe Dávila <filipedavila6@gmail.com>
2025-01-22 16:50:13 +0000

    Fix: overflow of loading sprinner

--- src/popup/router/pages/Loading.vue
+++ src/popup/router/pages/Loading.vue
@@ -87,7 +87,7 @@ export default {
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  overflow: auto;
+  overflow: hidden;
 }
 .loader {
   border: 16px solid transparent; /* Light grey */
