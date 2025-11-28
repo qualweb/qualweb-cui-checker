c7390f98f4caf8fee7db123a46aa9ce11d5a6407
Filipe <filipedavila6@gmail.com>
2025-11-28 16:51:21 +0000

    Config: settings for dev and prod and webpack configuration for dev, debug and prod.

--- webpack.config-production.js
+++ webpack.config-production.js
@@ -1,150 +1,166 @@
-const path = require('path');
+const path = require('node:path');
 const webpack = require('webpack');
 const ejs = require('ejs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const ExtReloader = require('webpack-ext-reloader');
 const { VueLoaderPlugin } = require('vue-loader');
-const { version } = require('./package.json');
+const { version } = require('./package.json'); 
 const TerserPlugin = require('terser-webpack-plugin');
+const fs = require('node:fs');
+const definitions = JSON.parse(fs.readFileSync("./definitions-prod.json", "utf-8"));
 
+// --- Configuration Object for Production ---
 const config = {
-  mode: 'production',
-  entry: {
-    background: './src/background/background.ts',
-    content: './src/content/content.ts',
-    sidebar: './src/sidebar/sidebar.ts',
-    options: './src/options/options.js',
-
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
-  resolve: {
-    alias: {
-   'vue': 'vue/dist/vue.esm-bundler.js'
+    mode: 'production', 
+    entry: {
+        background: './src/background/background.ts',
+        content: './src/content/content.ts',
+        sidebar: './src/sidebar/sidebar.ts',
+        options: './src/options/options.js',
     },
-    extensions: ['.ts', '.js', '.vue'],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin({
-      terserOptions: {
-        keep_classnames: true, 
-      },
-    })],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: { appendTsSuffixTo: [/\.vue$/] }
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-typescript', 
-            ],
-            
-          },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+
+        publicPath: '/', 
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js', 
+            "@microsoft/recognizers-text-number-with-unit": path.resolve(
+                __dirname,
+                "node_modules/@microsoft/recognizers-text-number-with-unit/dist/recognizers-text-number-with-unit.es5.js"
+            ),
         },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.sass$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader?indentedSyntax'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: '/images/',
-          emitFile: false,
+        extensions: ['.ts', '.js', '.vue'],
+    },
+    optimization :{
+        minimize: true,
+    minimizer: [
+        new TerserPlugin({
+          exclude: /cui\.js$|act\.js$|wcag\.js$|qwPage\.js$|util\.js$/,
+          terserOptions: {
+        compress: {
+          drop_console: true,    
+          drop_debugger: true,   
         },
+        format: {
+          comments: false      
+        }
       },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: '/fonts/',
-          emitFile: false,
-        },
-      },
-    ],
-  },
-  plugins: [
+      extractComments: false    
+        }),
+    ],},
     
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({ filename: '[name].css' }),
-    new CopyPlugin({
-      patterns: [
-      // { from: 'devtools.html', to: 'devtools.html', transform: transformHtml },
-      { from: './node_modules/@qualweb/qw-page/dist/qw-page.bundle.js', to: 'qwPage.js' },
-      { from: './node_modules/@qualweb/util/dist/__webpack/util.bundle.js', to: 'util.js' },
-      { from: './node_modules/@qualweb/act-rules/dist/__webpack/act.bundle.js', to: 'act.js' },
-      { from: './node_modules/@qualweb/cui-checks/dist/__webpack/cui.bundle.js', to: 'cui.js' },
-      { from: './node_modules/@qualweb/cui-checks/dist/__webpack/common-words-pt.txt', to: 'common-words-pt.txt' },
-      { from: './node_modules/@qualweb/wcag-techniques/dist/__webpack/wcag.bundle.js', to: 'wcag.js' },
-      
-      { from: 'src/locales/en.js', to: 'locales/en.js' },
-      { from: 'src/sidebar/evaluate.js', to: 'sidebar/evaluate.js' },
-      { from: 'src/sidebar/detect.js', to: 'sidebar/detect.js' },
-      { from: 'src/sidebar/interact.js', to: 'sidebar/interact.js' },
-      { from: 'src/sidebar/sidebar.html', to: 'sidebar/sidebar.html', transform:{ transformer:transformHtml , cache: true } },
-      { from: 'src/options/options.html', to: 'options/options.html', transform: transformHtml },
-      { 
-        from: 'src/icons', 
-        to: 'icons', 
-        globOptions: { 
-          ignore: ['**/icon.xcf'] 
-        } 
-      },
-      { from: 'src/sidebar/sidebar.html', to: 'sidebar/sidebar.html', transform: transformHtml },
-      { from: 'src/sidebar/sidebar.html', to: 'options/options.html', transform: transformHtml },
-      {
-        from: 'manifest.json',
-        to: 'manifest.json',
-      }
-    ]
-  }),
-  ],
+    devtool: false, 
+    
+    module: {
+        rules: [
+
+
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: { appendTsSuffixTo: [/\.vue$/] }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    compilerOptions: {
+                        compatConfig: {
+                            MODE: 2
+                        }
+                    },
+                }
+            },
+            {
+                test: /\.js$/,
+        
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env', { modules: false }]], 
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'] 
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.sass$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader?indentedSyntax'],
+            },
+
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+                type: 'asset/resource',
+                generator: {
+                  filename: 'images/[name][ext]'
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                type: 'asset/resource',
+                generator: {
+                  filename: 'fonts/[name][ext]'
+                }
+            },
+        ],
+    },
+    
+
+    plugins: [
+        new webpack.DefinePlugin({
+            APP_CONFIG: JSON.stringify(definitions),
+            '__VUE_OPTIONS_API__': JSON.stringify(true),
+            '__VUE_PROD_DEVTOOLS__': JSON.stringify(false), // Disable Devtools
+            '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false),
+            'process.env': {
+                NODE_ENV: '"production"',
+            },
+        }),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        
+        new CopyPlugin({
+            patterns: [
+                { from: './node_modules/@qualweb/qw-page/dist/qw-page.bundle.js', to: 'scripts/qwPage.js' },
+                { from: './node_modules/@qualweb/util/dist/__webpack/util.bundle.js', to: 'scripts/util.js' },
+                { from: './node_modules/@qualweb/act-rules/dist/__webpack/act.bundle.js', to: 'scripts/act.js' },
+                { from: './node_modules/@qualweb/cui-checks/dist/__webpack/cui.bundle.js', to: 'scripts/cui.js' },
+                { from: './node_modules/@qualweb/wcag-techniques/dist/__webpack/wcag.bundle.js', to: 'scripts/wcag.js' },
+                { from: './node_modules/@qualweb/cui-checks/dist/__webpack/common-words-pt.txt', to: 'resources/common-words-pt.txt' },
+                
+                { from: 'src/sidebar/evaluate.js', to: 'sidebar/evaluate.js' },
+                { from: 'src/sidebar/detect.js', to: 'sidebar/detect.js' },
+                { from: 'src/sidebar/interact.js', to: 'sidebar/interact.js' },
+                { from: 'prod/sidebar.html', to: 'sidebar/sidebar.html', transform: transformHtml },
+                { from: 'prod/options.html', to: 'options/options.html', transform: transformHtml },
+                
+                { 
+                    from: 'src/icons', 
+                    to: 'icons', 
+                    globOptions: { 
+                        ignore: ['**/icon.xcf'] 
+                    } 
+                },
+                { from: './prod/manifest.json', to: 'manifest.json' }
+            ]
+        }),
+    ],
 };
 
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-    })
-  );
-
 function transformHtml(content) {
-  return ejs.render(content.toString(), {
-    ...process.env,
-  });
+    return ejs.render(content.toString(), {
+        ...process.env,
+    });
 }
 
 module.exports = config;
