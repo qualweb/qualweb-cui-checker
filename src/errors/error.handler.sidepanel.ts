e77b0eb60473a3d53c431f9bf699c7e6987b8658
Filipe <filipedavila6@gmail.com>
2026-01-11 20:07:12 +0000

    fix(sidepanel): prevent navigation to error page on cancellation signals
    Improvment on handling system for sidepanel

--- src/errors/error.handler.sidepanel.ts
+++ src/errors/error.handler.sidepanel.ts
@@ -1,59 +1,51 @@
-import {  STATUS } from "../messaging/message-types";
-import { CRITICAL_ERRORS_RESET_SELECTORS, IErrorApiHandlerArgs, IErrorHandlerArgs, IErrorResponseHandlerArgs, SIDEPANEL_EXCEPTION_HANDLERS } from "./sidepanel/error.registry.sidepanel";
-import { APIError, APIErrorsWithDetails } from "./sidepanel/errors.class.sidepanel";
+import { Router } from "vue-router";
+import {  MessageResponseError, STATUS } from "../messaging/message-types";
+import { CRITICAL_ERRORS_RESET_SELECTORS,IErrorHandlerArgs,SIDEPANEL_EXCEPTION_HANDLERS } from "./sidepanel/error.registry.sidepanel";
 import { UI_MESSAGE_FRIENDLY_ERRORS } from "./sidepanel/errors.definitions.sidepanel";
 
 
 
-
-function defaultErrorHandler(args: IErrorHandlerArgs): void {
-    const name = args.error.name;
-    // if is type of Error, get error number
-    const errorNumber = args.error instanceof APIErrorsWithDetails ? args.error.error_number : undefined;
-    const errorSuffix = errorNumber ? `\nError - ${errorNumber}` : '';
-    const message =  UI_MESSAGE_FRIENDLY_ERRORS[name] + errorSuffix  || "An unexpected error occurred.";
-
-    if(args.callback) args.callback();
-    args.router.push({ name: 'error', query: { error: message  } });
+// Extract the error name from Error instance or Message of Error received
+function extractErrorName(args: IErrorHandlerArgs): string {
+    let name = '';
+    if (args.error instanceof Error) {
+        name = args.error.name;
+    } else {
+        name = args.error.code;
+    }
+    return name;
 }
 
-
-export  function  interceptErrors(result,router){
+/** Interceptor o Errors */
+export  function  interceptErrorResponse(result:MessageResponseError,router:Router): boolean {
        if (!result || result.status === STATUS.ERROR) {
-       handleErrorResponseSidepanel({ error: result, router: router });
+       defaultErrorHandler({ error: result, router: router });
         return true ;
       }
       return false;
 }
 
 export async function handleErrorSidepanel(errorArgs: IErrorHandlerArgs): Promise<void> {
-    const name =  errorArgs.error.name;  
-    const handler = SIDEPANEL_EXCEPTION_HANDLERS[name];
-    if(handler) {
-        handler(errorArgs);
+    const name = extractErrorName(errorArgs);
+    const customErrorHandler = SIDEPANEL_EXCEPTION_HANDLERS[name];
+    if(customErrorHandler) {
+       await customErrorHandler(errorArgs);
     } else {
-        defaultErrorHandler(errorArgs);
+        await defaultErrorHandler(errorArgs);
     }
+
 } 
 
-export async function handleApiErrorSidepanel(errorArgs: IErrorApiHandlerArgs): Promise<void> {
-    const error = errorArgs.apiError;
-    if (error instanceof APIError) {
-        if(errorArgs.callback) errorArgs.callback();
-        if (errorArgs.router) {
-            errorArgs.router.push({ name: 'error', query: { error: errorArgs.apiError.message } }); 
-        }
-    }
-}
-
-
-export async function handleErrorResponseSidepanel(errorArgs: IErrorResponseHandlerArgs): Promise<void> {
+export async function defaultErrorHandler(errorArgs: IErrorHandlerArgs): Promise<void> {
     const { error, storeDispatchCallback, callback, router } = errorArgs;
-    const name =  error.code;
+
+    const name = extractErrorName(errorArgs);
+
     if(CRITICAL_ERRORS_RESET_SELECTORS.has(name)) {
     if(storeDispatchCallback)
         await storeDispatchCallback();
     }
+
     const messageFriendly = UI_MESSAGE_FRIENDLY_ERRORS[name] ;
     const baseMessage = error.message || "An unexpected error occurred.";
     const message = messageFriendly || baseMessage ;
--- src/errors/sidepanel/error.registry.sidepanel.ts
+++ src/errors/sidepanel/error.registry.sidepanel.ts
@@ -1,25 +1,15 @@
 import { Router } from "vue-router";
-import { APIError } from "./errors.class.sidepanel";
 import { MessageResponseError } from "../../messaging/message-types";
 import { ERROR_CLASS_NAME_CONTENT } from "../content/errors.class.content";
 
 export interface IErrorHandlerArgs {
-    error: Error ;
+    error: Error | MessageResponseError ;
     showMessageUI?: Promise<void>;
-    callback?: () => void;
-    router:Router;
-}
-export interface IErrorApiHandlerArgs {
-    apiError: APIError;
-    callback?: () => void;
-    router?:Router;
-}
-export interface IErrorResponseHandlerArgs {
-    error: MessageResponseError ;
     callback?: () => void | Promise<void>;
     router:Router;
     storeDispatchCallback?: () => Promise<void>;
-}
+} 
+
 // Errors that make the sidepanel reset and reset storage selectors
 export const CRITICAL_ERRORS_RESET_SELECTORS = new Set<string>([
     ERROR_CLASS_NAME_CONTENT.ELEMENT_NOT_FOUND_ERROR,
@@ -34,7 +24,7 @@ export const CRITICAL_ERRORS_RESET_SELECTORS = new Set<string>([
     ERROR_CLASS_NAME_CONTENT.MESSAGE_SELECTOR_NOT_FOUND_ERROR,
 ]);
 
-export const SIDEPANEL_EXCEPTION_HANDLERS: Partial<Record<string, (args: IErrorHandlerArgs) => void>> = {
+export const SIDEPANEL_EXCEPTION_HANDLERS: Partial<Record<string, (args: IErrorHandlerArgs) => Promise<void>>> = {
     /*ActionDoesNotExistError: (_args) => {
         console.log("Handling ActionDoesNotExistError in side panel");
     },*/
